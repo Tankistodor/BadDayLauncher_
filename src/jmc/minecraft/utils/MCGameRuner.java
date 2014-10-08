f24cbf48659815ad8568c51c19d6da2c2ae9b75b
Tankistodor <tankistodor.tankistodor@gmail.com>
2014-10-09 00:09:31 +0400

    remove hash

--- src/jmc/minecraft/utils/MCGameRuner.java
+++ src/jmc/minecraft/utils/MCGameRuner.java
@@ -194,57 +194,6 @@ public class MCGameRuner {
 			params.add(ClientFolderPath + "assets");
 			
 		} else if (GlobalVar.fmlVersion.equals("1.7.10")) {
-			/* C:\Java\64\jre7\bin\javaw.exe -XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe.heapdump 
-			 * -Xmx1G -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:-UseAdaptiveSizePolicy -Xmn128M  -XX:MaxPermSize=512M 
-			 * -Djava.library.path=C:\Users\kolesnikovak\AppData\Roaming\.minecraft\versions\1.7.10-Forge10.13.1.1222\1.7.10-Forge10.13.1.1222-natives-4726770792780497 
-			 * -cp C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\net\minecraftforge\forge\1.7.10-10.13.1.1222\forge-1.7.10-10.13.1.1222.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\net\minecraft\launchwrapper\1.11\launchwrapper-1.11.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\org\ow2\asm\asm-all\5.0.3\asm-all-5.0.3.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\com\typesafe\akka\akka-actor_2.11\2.3.3\akka-actor_2.11-2.3.3.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\com\typesafe\config\1.2.1\config-1.2.1.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\org\scala-lang\scala-actors-migration_2.11\1.1.0\scala-actors-migration_2.11-1.1.0.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\org\scala-lang\scala-compiler\2.11.1\scala-compiler-2.11.1.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\org\scala-lang\plugins\scala-continuations-library_2.11\1.0.2\scala-continuations-library_2.11-1.0.2.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\org\scala-lang\plugins\scala-continuations-plugin_2.11.1\1.0.2\scala-continuations-plugin_2.11.1-1.0.2.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\org\scala-lang\scala-library\2.11.1\scala-library-2.11.1.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\org\scala-lang\scala-parser-combinators_2.11\1.0.1\scala-parser-combinators_2.11-1.0.1.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\org\scala-lang\scala-reflect\2.11.1\scala-reflect-2.11.1.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\org\scala-lang\scala-swing_2.11\1.0.1\scala-swing_2.11-1.0.1.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\org\scala-lang\scala-xml_2.11\1.0.2\scala-xml_2.11-1.0.2.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\net\sf\jopt-simple\jopt-simple\4.5\jopt-simple-4.5.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\lzma\lzma\0.0.1\lzma-0.0.1.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\com\mojang\realms\1.3.5\realms-1.3.5.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\org\apache\commons\commons-compress\1.8.1\commons-compress-1.8.1.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\org\apache\httpcomponents\httpclient\4.3.3\httpclient-4.3.3.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\commons-logging\commons-logging\1.1.3\commons-logging-1.1.3.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\org\apache\httpcomponents\httpcore\4.3.2\httpcore-4.3.2.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\java3d\vecmath\1.3.1\vecmath-1.3.1.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\net\sf\trove4j\trove4j\3.0.3\trove4j-3.0.3.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\com\ibm\icu\icu4j-core-mojang\51.2\icu4j-core-mojang-51.2.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\com\paulscode\codecjorbis\20101023\codecjorbis-20101023.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\com\paulscode\codecwav\20101023\codecwav-20101023.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\com\paulscode\libraryjavasound\20101123\libraryjavasound-20101123.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\com\paulscode\librarylwjglopenal\20100824\librarylwjglopenal-20100824.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\com\paulscode\soundsystem\20120107\soundsystem-20120107.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\io\netty\netty-all\4.0.10.Final\netty-all-4.0.10.Final.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\com\google\guava\guava\16.0\guava-16.0.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\org\apache\commons\commons-lang3\3.2.1\commons-lang3-3.2.1.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\commons-io\commons-io\2.4\commons-io-2.4.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\commons-codec\commons-codec\1.9\commons-codec-1.9.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\net\java\jinput\jinput\2.0.5\jinput-2.0.5.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\net\java\jutils\jutils\1.0.0\jutils-1.0.0.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\com\google\code\gson\gson\2.2.4\gson-2.2.4.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\com\mojang\authlib\1.5.16\authlib-1.5.16.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\org\apache\logging\log4j\log4j-api\2.0-beta9\log4j-api-2.0-beta9.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\org\apache\logging\log4j\log4j-core\2.0-beta9\log4j-core-2.0-beta9.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\org\lwjgl\lwjgl\lwjgl\2.9.1\lwjgl-2.9.1.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\org\lwjgl\lwjgl\lwjgl_util\2.9.1\lwjgl_util-2.9.1.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\libraries\tv\twitch\twitch\5.16\twitch-5.16.jar;
-			 * C:\Users\kolesnikovak\AppData\Roaming\.minecraft\versions\1.7.10-Forge10.13.1.1222\1.7.10-Forge10.13.1.1222.jar net.minecraft.launchwrapper.Launch 
-			 * --username Tankistodor --version 1.7.10-Forge10.13.1.1222 --gameDir C:\Users\kolesnikovak\AppData\Roaming\.minecraft 
-			 * --assetsDir C:\Users\kolesnikovak\AppData\Roaming\.minecraft\assets --assetIndex 1.7.10 
-			 * --uuid 1877a626d9b04afe939abe17db54b2a6 --accessToken 30d98fb9d99e4b6498de82c7cd66df6d --userProperties {} --userType legacy --tweakClass cpw.mods.fml.common.launcher.FMLTweaker
-			 */
 			
 			params.add("-cp");
 			params.add(
@@ -318,8 +267,6 @@ public class MCGameRuner {
 			
 			params.add("--uuid");
 			if (Online) {
-				//params.add(UUID.randomUUID().toString());
-				//params.add(GlobalVar.uuid);
 				params.add(GlobalVar.uuid);
 			} else {
 				params.add("0000");
